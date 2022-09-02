e88ce87be3f2d3c86e25e70a00ac3ce64b3223c0
Dragon1207 <smileboth0805@outlook.com>
2022-09-02 07:13:56 +0300

    todo list and admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Motor::Admin => '/motor_admin'
+  resources :todo_lists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
