8a6ff961f17b301fe36bcf63e878b66ecf47d50d
Dragon1207 <smileboth0805@outlook.com>
2022-09-24 13:23:46 +0300

    remove default homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount Motor::Admin => '/motor_admin'
+  root to: 'main#index'
   resources :todo_lists
   resources :dogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
