f67144a8e9d8278f808b77e8b387c70d11d6a74e
Dragon1207 <smileboth0805@outlook.com>
2022-09-27 22:34:21 +0300

    remove unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   mount Motor::Admin => '/motor_admin'
   root to: 'main#index'
   get 'guides', to: 'guides#show'
-  resources :todo_lists
-  resources :dogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
