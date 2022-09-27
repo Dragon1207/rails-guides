4071b961b14cbd5f9e37281b917bb6f04f8e2c3d
Dragon1207 <smileboth0805@outlook.com>
2022-09-27 22:28:27 +0300

    complete home page and guide page basically

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount Motor::Admin => '/motor_admin'
   root to: 'main#index'
+  get 'guides', to: 'guides#show'
   resources :todo_lists
   resources :dogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
