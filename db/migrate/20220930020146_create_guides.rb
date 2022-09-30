ebb80ad0d3ab3547c2413438b67d48b4efb05353
Dragon1207 <smileboth0805@outlook.com>
2022-09-30 09:40:17 +0300

    update migration files

--- db/migrate/20220930020146_create_guides.rb
+++ db/migrate/20220930020146_create_guides.rb
@@ -2,7 +2,7 @@ class CreateGuides < ActiveRecord::Migration[7.0]
   def change
     create_table :guides do |t|
       t.string :title
-
+      t.string :section
       t.timestamps
     end
   end
--- db/migrate/20220930051950_add_section_to_guides.rb
+++
@@ -1,5 +0,0 @@
-class AddSectionToGuides < ActiveRecord::Migration[7.0]
-  def change
-    add_column :guides, :section, :string
-  end
-end
